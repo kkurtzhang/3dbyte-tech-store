30ddd5e3b345873ee417cd5e7262ea93e5cf2dfb
kkurtzhang <kurtbzhang@gmail.com>
2026-01-16 18:42:56 +1100

    fix(storefront-v3): add DialogTitle for accessibility in CommandDialog

--- apps/storefront-v3/src/components/ui/command.tsx
+++ apps/storefront-v3/src/components/ui/command.tsx
@@ -4,6 +4,7 @@ import * as React from "react";
 import { type DialogProps } from "@radix-ui/react-dialog";
 import { Command as CommandPrimitive } from "cmdk";
 import { Search } from "lucide-react";
+import * as DialogPrimitive from "@radix-ui/react-dialog";
 
 import { cn } from "@/lib/utils";
 import { Sheet, SheetContent } from "@/components/ui/sheet";
@@ -31,6 +32,9 @@ const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
   return (
     <Sheet {...props}>
       <SheetContent side="top" className="p-0">
+        <DialogPrimitive.Title className="sr-only">
+          Command Palette
+        </DialogPrimitive.Title>
         <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
           {children as any}
         </Command>
