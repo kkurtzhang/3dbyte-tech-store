f623f1f06d994301aff38e98850fc78d03dbbd4f
kkurtzhang <164311565+kkurtzhang@users.noreply.github.com>
2026-03-28 10:59:19 +1100

    Chore/cms-content type collection modification (#51)
    * chore(cms): add medusa_collection_id into schema
    
    * feat(cms): add collection unpublish endpoint and sync metadata support

--- apps/cms/src/api/collection/controllers/collection.ts
+++ apps/cms/src/api/collection/controllers/collection.ts
@@ -2,6 +2,25 @@
  * collection controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::collection.collection');
+export default factories.createCoreController(
+  "api::collection.collection",
+  ({ strapi }) => ({
+    async unpublish(ctx) {
+      const { documentId } = ctx.params;
+
+      if (!documentId || typeof documentId !== "string") {
+        return ctx.badRequest("documentId is required");
+      }
+
+      const result = await strapi
+        .service("api::collection.collection")
+        .unpublish(documentId);
+
+      ctx.body = {
+        data: result,
+      };
+    },
+  }),
+);
