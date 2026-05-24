b062df24cf415d69b0cdf01921b437322e10ebcb
kkurtzhang <kurtbzhang@gmail.com>
2026-05-24 11:46:03 +1000

    fix(cms): allow public product document lookup

--- apps/cms/src/api/product-document/controllers/product-document.ts
+++ apps/cms/src/api/product-document/controllers/product-document.ts
@@ -6,4 +6,25 @@ import { factories } from "@strapi/strapi";
 
 export default factories.createCoreController(
   "api::product-document.product-document",
+  () => ({
+    async find(ctx) {
+      const query = ctx.query as Record<string, unknown>;
+      const filters =
+        query.filters && typeof query.filters === "object"
+          ? (query.filters as Record<string, unknown>)
+          : {};
+
+      ctx.query = {
+        ...query,
+        filters: {
+          ...filters,
+          is_public: {
+            $eq: true,
+          },
+        },
+      };
+
+      return super.find(ctx);
+    },
+  }),
 );
--- apps/cms/src/api/product-document/routes/product-document.ts
+++ apps/cms/src/api/product-document/routes/product-document.ts
@@ -6,4 +6,11 @@ import { factories } from "@strapi/strapi";
 
 export default factories.createCoreRouter(
   "api::product-document.product-document",
+  {
+    config: {
+      find: {
+        auth: false,
+      },
+    },
+  },
 );
